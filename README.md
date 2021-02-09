e9acdfd7c5d1838cf7420456e75d604a2fe8026e
Stephen Phillips <stephenphillips@Stephens-MacBook-Pro.local>
2021-02-08 18:35:15 -0800

    start

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # newMountainTemplate
 other mountains
+
+
+https://www.epicpass.com/region/other-international.aspx
+
+https://www.ikonpass.com/en/destinations
---
+++ mountains.js
@@ -0,0 +1,9 @@
+{
+    "ABasin": {
+    "country": "Japan"
+    "pass": "epic"
+      "latitude": 39.6425,
+        "longitude": -105.8719,
+          "name": "Arapahoe Basin"
+    },
+}
