18d68a7267e26d6f35eff890109802d2f59aefed
Stephen Phillips <stephenphillips@Stephens-MacBook-Pro.local>
2021-02-08 18:46:44 -0800

    another

--- allmountains.js
+++ allmountains.js
@@ -1,3 +1,4 @@
 const allMountains = [
-    "Perisher"
+    "Perisher",
+    "FallsCreek"
   ]
--- mountains.js
+++ mountains.js
@@ -6,4 +6,11 @@
         "longitude": 148.4117,
           "name": "Perisher"
     },
+    "FallsCreek": {
+    "country": "Australia"
+    "pass": "Epic",
+      "latitude": -36.8659,
+        "longitude": 147.2870,
+          "name": "Falls Creek"
+    },
 }
